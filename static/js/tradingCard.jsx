88101b01587b6bfb432360420e3c707fc601c840
obienias <olga.julia.bienias@gmail.com>
2023-02-17 16:35:09 -0800

    Finished Lab

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -82,7 +82,59 @@ function TradingCardContainer() {
     );
   }
 
-  return <div className="grid">{tradingCards}</div>;
+  return ( 
+    <React.Fragment>
+      <AddTradingCard />
+      <h2>Trading Cards</h2>
+      <div className="grid">{tradingCards}</div>
+    </React.Fragment>
+);
 }
 
 ReactDOM.render(<TradingCardContainer />, document.getElementById('container'));
+
+function AddTradingCard(props) {
+  const [name, setName] = React.useState("");
+  const [skill, setSkill] = React.useState("");
+  function addNewCard() {
+    fetch("/add-card", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ 'name': name, 'skill': skill }),
+    })
+      .then((response) => response.json())
+      .then((jsonResponse) => { 
+        const cardAdded = jsonResponse.cardAdded;
+        props.addCard(cardAdded);
+      });
+  }
+  
+  return (
+    <React.Fragment>
+      <h2>Add New Trading Card</h2>
+      <label htmlFor="nameInput">Name</label>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="nameInput"
+        style={{ marginLeft: "5px" }}
+      ></input>
+      <label
+        htmlFor="skillInput"
+        style={{ marginLeft: "10px", marginRight: "5px" }}
+      >
+        Skill
+      </label>
+      <input
+        value={skill}
+        onChange={(event) => setSkill(event.target.value)}
+        id="skillInput"
+      ></input>
+      <button style={{ marginLeft: "10px" }} onClick={addNewCard}>
+        Add
+      </button>
+    </React.Fragment>
+  );
+}
